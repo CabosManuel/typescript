082e68446f4b0b60a95a164b2df14469e8474844
CabosManuel <cabos.apolaya@gmail.com>
2024-08-23 23:06:48 -0500

    Arrays

---
+++ src/07-arrays.ts
@@ -0,0 +1,16 @@
+(() => {
+  const numbers = [1,2,4,5,6,7,1, '13']; // number[]
+  const types = [1,2,3,'4',true]; // (string | number | boolean)[]
+
+  // prices.push('123'); // Error
+  // prices.push(true); // Error
+  // prices.push({}); // Error
+  numbers.push(123);
+
+  let mixed: (string | number | boolean | object)[] = [false, 1];
+  mixed.push('2');
+  mixed.push({});
+
+  let even = [2,4,6,8];
+  even.map(item => item * 2);
+})();
